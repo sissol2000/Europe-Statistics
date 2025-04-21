25183a47a33c6003d417e292cbe3534967c85fca
sissol2000 <umacontafalsificadadomt@gmail.com>
2025-04-21 16:47:52 +0100

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,553 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "Não faz parte da União Europeia",
+    state_color: "#b4b4b4",
+    state_hover_color: "#768393",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "10",
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    "1": {},
+    "2": {},
+    "3": {},
+    "4": {},
+    "5": {},
+    "6": {},
+    "7": {},
+    "8": {},
+    "9": {},
+    "10": {},
+    "11": {},
+    AL: {
+      name: "Albania",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    AM: {
+      name: "Armenia",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    AT: {
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  3,29%",
+      name: "Áustria"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    BE: {
+      name: "Bélgica",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  3,32%"
+    },
+    BG: {
+      name: "Bulgária",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  0,79%"
+    },
+    BY: {
+      name: "Belarus",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    CH: {
+      name: "Switzerland",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    CY: {
+      name: "Chipre",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  0,68%"
+    },
+    CZ: {
+      name: "República Checa",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,97%"
+    },
+    DE: {
+      name: "Alemanha",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  3,11%"
+    },
+    DK: {
+      name: "Dinamarca",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  3,03%"
+    },
+    EE: {
+      name: "Estónia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,75%"
+    },
+    ES: {
+      name: "Espanha",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,47%"
+    },
+    FI: {
+      name: "Finlândia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  3,09%"
+    },
+    FR: {
+      name: "França",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  2,34%"
+    },
+    GB: {
+      name: "United Kingdom",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    GE: {
+      name: "Georgia",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    GR: {
+      name: "Grécia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,33%"
+    },
+    HR: {
+      name: "Croácia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,29%"
+    },
+    HU: {
+      name: "Hungria",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,39%"
+    },
+    IE: {
+      name: "Irlanda",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,13%"
+    },
+    IS: {
+      name: "Iceland",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    IT: {
+      name: "Itália",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,45%"
+    },
+    LT: {
+      name: "Lituânia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,11%"
+    },
+    LU: {
+      name: "Luxemburgo",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  2,79%"
+    },
+    LV: {
+      name: "Letónia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  0,96%"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    ME: {
+      name: "Montenegro",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    MK: {
+      name: "Macedonia",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    NL: {
+      name: "Países Baixos",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  2,3%"
+    },
+    NO: {
+      name: "Norway",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    PL: {
+      name: "Polónia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,57%"
+    },
+    PT: {
+      name: "Portugal",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  1,54%"
+    },
+    RO: {
+      name: "Roménia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  0,52%"
+    },
+    RS: {
+      name: "Serbia",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    SE: {
+      name: "Suécia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  3,57%"
+    },
+    SI: {
+      name: "Eslovénia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  2,43%"
+    },
+    SK: {
+      name: "Eslováquia",
+      inactive: "no",
+      color: "#003399",
+      hover_color: "#FFCC00",
+      description: "Parte do PIB para a ciência:  0,88%"
+    },
+    TR: {
+      name: "Turkey",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    },
+    UA: {
+      name: "Ukraine",
+      color: "#b4b4b4",
+      hover_color: "#768393"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "54.687157",
+      lng: "25.279652",
+      name: "Vilnius"
+    },
+    "1": {
+      lat: 38.726,
+      lng: -9.15,
+      name: "Lisboa"
+    },
+    "2": {
+      lat: "41.327545",
+      lng: "19.818889",
+      name: "Tirana"
+    },
+    "3": {
+      lat: "52.520007",
+      lng: "13.404954",
+      name: "Berlim"
+    },
+    "4": {
+      lat: "48.208174",
+      lng: "16.373819",
+      name: "Viena"
+    },
+    "5": {
+      lat: "50.850346",
+      lng: "4.351721",
+      name: "Bruxelas"
+    },
+    "6": {
+      lat: "53.900000",
+      lng: "27.566667",
+      name: "Minsk"
+    },
+    "7": {
+      lat: "43.856259",
+      lng: "18.413076",
+      name: "Sarajevo"
+    },
+    "8": {
+      lat: "42.697708",
+      lng: "23.324237",
+      name: "Sófia"
+    },
+    "9": {
+      lat: "35.166667",
+      lng: "33.366667",
+      name: "Nicósia"
+    },
+    "10": {
+      lat: "45.815400",
+      lng: "15.966568",
+      name: "Zagrebe"
+    },
+    "11": {
+      lat: "55.676098",
+      lng: "12.568337",
+      name: "Copenhaga"
+    },
+    "12": {
+      lat: "48.148596",
+      lng: "17.107748",
+      name: "Bratislava"
+    },
+    "13": {
+      lat: "46.051081",
+      lng: "14.505133",
+      name: "Liubliana"
+    },
+    "14": {
+      lat: "40.416775",
+      lng: "-3.703790",
+      name: "Madrid"
+    },
+    "15": {
+      lat: "59.437000",
+      lng: "24.753575",
+      name: "Tallin"
+    },
+    "16": {
+      lat: "60.169521",
+      lng: "24.935450",
+      name: "Helsínquia"
+    },
+    "17": {
+      lat: "48.856613",
+      lng: "2.352222",
+      name: "Paris"
+    },
+    "18": {
+      lat: "41.697102",
+      lng: "44.773674",
+      name: "Tbilisi"
+    },
+    "19": {
+      lat: "37.983810",
+      lng: "23.727539",
+      name: "Atenas"
+    },
+    "20": {
+      lat: "47.497912",
+      lng: "19.040235",
+      name: "Budapeste"
+    },
+    "21": {
+      lat: "53.349805",
+      lng: "-6.260310",
+      name: "Dublin"
+    },
+    "22": {
+      lat: "64.135486",
+      lng: "-21.895411",
+      name: "Reiquiavique"
+    },
+    "23": {
+      lat: "41.902783",
+      lng: "12.496366",
+      name: "Roma"
+    },
+    "24": {
+      lat: "56.949649",
+      lng: "24.105186",
+      name: "Riga"
+    },
+    "25": {
+      lat: "49.6117",
+      lng: "6.1319",
+      name: "Luxemburgo"
+    },
+    "27": {
+      lat: "41.998101",
+      lng: "21.425436",
+      name: "Escópia"
+    },
+    "28": {
+      lat: "47.0105",
+      lng: "28.8638",
+      name: "Chisinau"
+    },
+    "29": {
+      lat: "42.441761",
+      lng: "19.263571",
+      name: "Podgorica"
+    },
+    "30": {
+      lat: "59.913868",
+      lng: "10.752245",
+      name: "Oslo"
+    },
+    "31": {
+      lat: "52.367573",
+      lng: "4.904138",
+      name: "Amesterdão"
+    },
+    "34": {
+      lat: "52.229676",
+      lng: "21.012229",
+      name: "Varsóvia"
+    },
+    "36": {
+      lat: "51.507351",
+      lng: "-0.127758",
+      name: "Londres"
+    },
+    "38": {
+      lat: "50.075538",
+      lng: "14.437800",
+      name: "Praga"
+    },
+    "39": {
+      lat: "44.426767",
+      lng: "26.102538",
+      name: "Bucareste"
+    },
+    "40": {
+      lat: "44.812500",
+      lng: "20.461200",
+      name: "Belgrado"
+    },
+    "43": {
+      lat: "59.329323",
+      lng: "18.068581",
+      name: "Estocolmo"
+    },
+    "44": {
+      lat: "46.948094",
+      lng: "7.447444",
+      name: "Berna"
+    },
+    "45": {
+      lat: "50.450001",
+      lng: "30.523333",
+      name: "Kiev"
+    },
+    "46": {
+      lat: "39.9199",
+      lng: "32.8543",
+      name: "Ancara"
+    },
+    "47": {
+      lat: "42.000000",
+      lng: "21.433333",
+      name: "Pristina"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
